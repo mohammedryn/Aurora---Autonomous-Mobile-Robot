75aca48e1b24d7bd09755feb6f704a0147390f45
mohammedryn <mohammedrah1289@gmail.com>
2026-06-17 21:02:03 +0530

    feat(docker): add sim deps, GPU device, fix AMR workspace volume path

--- docker/run_sim.sh
+++ docker/run_sim.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 set -e
+
+xhost +local:docker 2>/dev/null || true
+
 docker run -it --rm \
-  --env DISPLAY=$DISPLAY \
+  --env DISPLAY="${DISPLAY}" \
   --env ROS_DOMAIN_ID=42 \
+  --env GZ_SIM_RESOURCE_PATH="/root/.gz/fuel/models" \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
-  -v "$HOME/amr/ros2_ws":/amr_ws \
+  --device /dev/dri \
+  -v "${HOME}/AMR/ros2_ws":/amr_ws \
+  -v "${HOME}/.gz/fuel":/root/.gz/fuel \
   --network host \
   amr_sim:latest \
-  ros2 launch amr_bringup sim.launch.py
+  bash -c "
+    source /opt/ros/jazzy/setup.bash && \
+    source /amr_ws/install/setup.bash && \
+    ros2 launch amr_bringup sim.launch.py
+  "
